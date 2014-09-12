4eebdce109ef74111e90a8203536636270219873
Andrew Timmins <andrew.timmins@interservefm.com>
2014-09-12 17:17:55 +0100

    Initial Commit

---
+++ interservelabs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet"  type="text/css" href="../css/mainui.css" />
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+        <!-- jQuery UI--> 
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+        <script src="../script/ui.js"></script>
+    </head>
+    <body>
+hello
+    </body>
+</html>
