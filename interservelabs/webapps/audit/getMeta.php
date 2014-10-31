34e3713628fde2e10a98013ef4bd0f00c9f75336
TIMMINSA <andrew.timmins@interservefm.com>
2014-10-31 09:12:00 +0000

    Daily Commit

--- interservelabs/webapps/audit/getMeta.php
+++ interservelabs/webapps/audit/getMeta.php
@@ -1,12 +1,9 @@
 <?php
 
-
-
-
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://sitmaximo.bbc-irv.co.uk/maxrest/rest/os/'.$_GET['OS'].'?_lid=timminsa&_lpwd=maximo'.$_GET['filterString']
+    CURLOPT_URL => 'http://sitmaximo.bbc-irv.co.uk/maxrest/rest/os/INTLABSMETA?_lid=timminsa&_lpwd=maximo&objectname='.$_GET['object']
 ));
 
 $resp = curl_exec($curl);
